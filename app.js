950777bc5644b8a6201924eb1e856f52be2623f4
Mihir Kumar <mihir.k@myclnq.co.in>
2024-09-24 17:10:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = require('./routers/router');
+
+const app = express();
+
+
+const PORT = 3000;
+
+app.use(express.json())
+
+app.use('/', router);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
